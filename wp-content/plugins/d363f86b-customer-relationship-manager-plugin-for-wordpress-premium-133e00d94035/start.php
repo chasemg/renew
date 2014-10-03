e9e67bb4859be99187ec970e0973c1e92d50389b
fortislogo <fortislogo@hotmail.com>
2014-10-04 04:43:43 +0800

    move git to public_html

--- wp-content/plugins/d363f86b-customer-relationship-manager-plugin-for-wordpress-premium-133e00d94035/start.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Add the Autoloader
-$loader = new TPC_Autoloader_LoaderClass( "TPCP", dirname( __FILE__ ) );
-$loader->register( );
--- wp-content/themes/FoundationPress-master/parts/top-bar.php
+++
@@ -1,9 +0,0 @@
-<div  id='sticky-nav' class="top-bar-container contain-to-grid show-for-medium-up">
-    <nav class="top-bar" data-topbar="">
-        <div class="top-bar-section">
-            <div class="nav-left-menu"><?php foundationPress_top_bar_l(); ?></div>
-			<div class='nav_logo'><a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png"></a></div>
-            <div class="nav-right-menu"><?php foundationPress_top_bar_r(); ?></div>
-        </div>
-    </nav>
-</div>
