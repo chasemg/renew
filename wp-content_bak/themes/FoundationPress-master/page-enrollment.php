c6efdd8f6718e1315777ba1400aee78ba23f4b03
Celso <fortislogo@hotmail.com>
2014-10-15 19:32:07 +0000

    fixed untracked files

---
+++ wp-content_bak/themes/FoundationPress-master/page-enrollment.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Template Name: Enrollment
+*/
+?>
+
+<?php get_header(); ?>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<?php error_reporting(E_ALL); ?>
+
+<?php if ( has_post_thumbnail() ) { ?>
+<div class="header_image">
+	<div id="featured"><?php the_post_thumbnail(); ?></div>
+	
+</div>
+<div class="header_map" onload="initialize()">
+	<div id="map_canvas"></div>
+</div>
+<?php } ?>
+
+<div class="row_container">
+	<div class="row">
+		<div class="small-12 columns" role="main">
+
+		<?php do_action('foundationPress_before_content'); ?>
+		<?php while (have_posts()) : the_post(); ?>
+			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+				<?php do_action('foundationPress_page_before_entry_content'); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+				<div>
+				<div class="enroll" id="form_one">
+					  <div>Address</div>
+					  <input type="text" id="street" value="609 E Thatcher Way">
+					   <div>City</div>
+					  <input type="text" id="city" value="Midvale">
+					  <div>State</div>
+					  <input type="text" id="state" value="UT">
+					  <div>Zip</div>
+					  <input type="text" id="zip" value="84047">
+					<button id="validate_address">Validate</button>
+				</div>
+				<div class="enroll" id="form_two">
+					<div id="address_validated"></div>
+				
+				  <div id="results">
+					<table>
+					  <tr><td align="right">Valid:</td><td><input type="text" id="valid" size="60"/></td></tr>
+					  <tr><td align="right">Matched:</td><td><input type="text" id="type" size="60"/></td></tr>
+					  <tr><td align="right">Result:</td><td><input type="text" id="result" size="60"/></td></tr>
+					  <tr><td align="right">Lat/Long:</td><td><input type="text" id="latlong" size="60"/></td></tr>
+					</table>
+				  </div>
+				</div>
+				<div class="enroll" id="form_three">
+				
+				</div>	
+				</div>				
+				<footer>
+					<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
+					<p><?php the_tags(); ?></p>
+				</footer>
+
+			</article>
+		<?php endwhile;?>
+
+		<?php do_action('foundationPress_after_content'); ?>
+
+		</div>
+		<?php get_sidebar(); ?>
+		
+	</div>
+</div>
+<?php get_footer(); ?>
---
+++ wp-content_bak/themes/FoundationPress-master/parts/address_validation.php
@@ -0,0 +1,75 @@
+<?php
+
+$html = '';
+
+if($_POST['street'] && $_POST['city'] && $_POST['state'] && $_POST['zip'])	{
+	$html .= "<input type='hidden' value='".$_POST['street']."' id='street_verified'>";
+	$html .= "<input type='hidden' value='".$_POST['city']."' id='city_verified'>";
+	$html .= "<input type='hidden' value='".$_POST['state']."' id='state_verified'>";
+	$html .= "<input type='hidden' value='".$_POST['zip']."' id='zip_verified'>";
+?>
+<script>
+  var geocoder, map, marker;
+  var defaultLatLng = new google.maps.LatLng(30,0);
+ 
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var mapOptions = {
+      zoom: 0,
+      center: defaultLatLng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(
+      document.getElementById("map_canvas"),
+      mapOptions
+    );
+    marker = new google.maps.Marker();
+  }	
+initialize();  
+  function validate() {
+    var street = $('#street_verified').val();
+	var city = $('#city_verified').val();
+	var state = $('#state_verified').val();
+	var zip = $('#zip_verified').val();
+	
+	var address = street+" ,"+city+", "+state+" "+zip;
+	//alert(address);
+    geocoder.geocode({'address': address }, function(results, status) {
+      switch(status) {
+        case google.maps.GeocoderStatus.OK:
+          document.getElementById('valid').value = 'YES';
+          document.getElementById('type').value = results[0].types[0];
+          document.getElementById('result').value = results[0].formatted_address;
+          mapAddress(results[0]);
+		  var latitude = results[0].geometry.location.lat();
+		  var longitude = results[0].geometry.location.lng();
+		  document.getElementById('latlong').value = latitude+", "+longitude;
+          break;
+        case google.maps.GeocoderStatus.ZERO_RESULTS:
+          document.getElementById('valid').value = 'NO';
+          break;
+        default:
+          alert("An error occured while validating this address")
+      }
+    });
+  }
+ 
+
+ 
+  function mapAddress(result) {
+    marker.setPosition(result.geometry.location);
+    marker.setMap(map);
+    map.fitBounds(result.geometry.viewport);
+  }
+  validate();
+  
+</script>	
+<?php
+} else {
+	$html .= 0;
+}
+
+echo $html;
+
+
+?>
