cf8314ad0ec538dee4e3851cea8c70fa7f9842d3
BlueZippo <michael.tiano@gmail.com>
2014-12-20 01:55:54 +0800

    Updated app.css, foundation.php & user_dashboard.php

--- wp-content/themes/FoundationPress-master/library/foundation.php
+++ wp-content/themes/FoundationPress-master/library/foundation.php
@@ -135,7 +135,7 @@ function dashboard($user_id) {
 		echo '<div class="dashboard_icons" id="schedule"><img src="' . get_template_directory_uri() . '/assets/img/dashboard/sched_icon.png"></div>';
 		echo '<div class="dashboard_bottom_icons">';
 		echo '<div class="dashboard_icons" id="settings"><img src="' . get_template_directory_uri() . '/assets/img/dashboard/settings_icon.png"></div>';
-		echo '<div class="dashboard_icons"><a href="'. wp_logout_url('index.php') .'"><img src="' . get_template_directory_uri() . '/assets/img/dashboard/logout_icon.png"></a></div>';
+		echo '<div class="dashboard_icons" id="logout"><a href="'. wp_logout_url('index.php') .'"><img src="' . get_template_directory_uri() . '/assets/img/dashboard/logout_icon.png"></a></div>';
 		echo '</div>';
 	} 
 
