218075985eddf17869d094d5b75b1dc3875467db
griff4594 <kgriffiths@chasemg.com>
2015-01-07 11:58:36 -0700

    patient search fix

--- wp-content/themes/FoundationPress-master/dashboard/patient_list.php
+++ wp-content/themes/FoundationPress-master/dashboard/patient_list.php
@@ -6,7 +6,7 @@ URL: http://chasemg.com
 include "db_include.php";
 $html = '';
 $id = $_POST['id'];
-$patients = $pdb->get_results("SELECT * FROM ".$wpdb->prefix. "patients AS p INNER JOIN ".$wpdb->prefix. "users AS u ON u.ID=p.user_id WHERE p.doctor='$id'");
+$patients = $pdb->get_results("SELECT * FROM ".$wpdb->prefix. "patients AS p WHERE p.doctor='$id'");
 $html .= "<ul id='patients'>";
 foreach($patients as $patient)	{
 	$html .= "<li class='patient' id='".$patient->user_id."'><div class='search_image'><img src='".get_template_directory_uri()."/dashboard/profile_photos/".$patient->image."'></div><div class='patient_search_name'>".$patient->display_name."</div></li>";
